42331b8e1d35e6ec6750220accc5096cf3331e7c
SaidLeben <rsmartinez@bside.com.mx>
2022-05-24 12:32:57 -0500

    Enable ApplicationInsightsTelemetry

--- customTelemetryAI/Program.cs
+++ customTelemetryAI/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+/*Enables AI telemtry collection*/
+builder.Services.AddApplicationInsightsTelemetry();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
